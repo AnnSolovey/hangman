cd6c28a144ef6eb2fc0d3896aa31b78e94b8520b
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2023-09-20 12:04:18 +0300

    Update Gallows.py

--- Project/Gallows.py
+++ Project/Gallows.py
@@ -1,4 +1,6 @@
 from random import choice
+from time import sleep
+from os import system
 words = []
 
 f = open('dictionary.txt')
@@ -8,54 +10,61 @@ for i in f.readlines():
     words.append(i)
 f.close()
 
-word = choice(words)
-alphabet = []
+while True:
+    word = choice(words)
+    alphabet = []
 
-print("You have 11 life")
-letters =[]
-for i in range(len(word)):
-    letters.append('_')
-print(''.join(letters))
-life = 11
-game = True
-let =0
-end =0
-# слова
-while game and life>0:
-    letter = input("Enter a word or a letter: ")
-    letter = letter.lower()
-    if letter in alphabet:
-        print("This letter was")
-    elif len(letter)>1:
-        # правильные слова
-        if letter == word:
-            game = False
-        else:
-            # неправильные слова
-            life -= 1
-            print("You have",life,'life')
-    else:
-        alphabet.append(letter)
-        # правильные буквы
-        for i in range(len(word)):
-            if letter == word[i]:
-                letters[i] = letter
-                end +=1
-            # неправильные буквы
+    print("You have 11 life")
+    letters =[]
+    for i in range(len(word)):
+        letters.append('_')
+    print(''.join(letters))
+    life = 11
+    game = True
+    let =0
+    end =0
+    # слова
+    while game and life>0:
+        letter = input("Enter a word or a letter: ")
+        letter = letter.lower()
+        system("cls")
+        if letter in alphabet:
+            print("This letter was")
+        elif len(letter)>1:
+            # правильные слова
+            if letter == word:
+                game = False
             else:
-                let+=1
-                if let == len(word):
-                    life -= 1
-                    let =0
-                    print("You have", life, 'life')
-        print(''.join(letters))
-        let = 0
-    # конец цикла
-    if end == len(word):
-        game = False
-# конец
-print("This word is", word)
-if life == 0:
-    print("Game over")
-else:
-    print("You win!")
+                # неправильные слова
+                life = 0
+        else:
+            alphabet.append(letter)
+            # правильные буквы
+            for i in range(len(word)):
+                if letter == word[i]:
+                    letters[i] = letter
+                    end +=1
+                # неправильные буквы
+                else:
+                    let+=1
+                    if let == len(word):
+                        life -= 1
+                        let =0
+                        print("You have", life, 'life')
+            print(''.join(letters))
+            let = 0
+        # конец цикла
+        if end == len(word):
+            game = False
+    # конец
+    print("This word is", word)
+    if life == 0:
+        print("Game over")
+    else:
+        print("You win!")
+    sleep(5)
+    system("cls")
+    print("Do you want to play again?")
+    ans = input(">>> ")
+    if ans.lower() == "no":
+        break
